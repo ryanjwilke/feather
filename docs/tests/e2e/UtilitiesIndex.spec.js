2137ef275ecd910da933c36f16d7504f06b29850
Ryan Wilke <ryanwilke@gmail.com>
2019-11-21 01:40:52 +0100

    Expand the Test Workflows (#18)
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * update snapshots
    
    * Update package.json
    
    * working on the test caches
    
    * Update production.yml
    
    * Update production.yml
    
    * Update UtilitiesIndex.spec.js

--- docs/tests/e2e/UtilitiesIndex.spec.js
+++ docs/tests/e2e/UtilitiesIndex.spec.js
@@ -3,6 +3,6 @@ import { snapshot } from './utils/snapshot.js'
 describe('UtilitiesIndex', () => {
   it('renders correctly', async () => {
     await page.goto('http://localhost:8080/utilities/')
-    await snapshot(page, 'Utilities Index')
+    await snapshot(page, 'Utilities::Index')
   })
 })
