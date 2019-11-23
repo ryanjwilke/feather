7c319bda1853ded41ba4cd5d56b28c2d8ca822e4
Ryan Wilke <ryanwilke@gmail.com>
2019-11-23 18:39:38 +0100

    Working on the search box

--- docs/src/layout/app-header/app-header.js
+++ docs/src/layout/app-header/app-header.js
@@ -3,6 +3,12 @@ import css from './app-header.css.yml'
 export default {
   name: 'app-header',
 
+  methods: {
+    keyup(event) {
+      this.$root.$emit('search', event.target.value)
+    },
+  },
+
   data() {
     return {
       css: css,
--- docs/src/layout/app-header/app-header.vue
+++ docs/src/layout/app-header/app-header.vue
@@ -1,8 +1,9 @@
 <template>
-  <header :class="css.header">
-    <div :class="css.border">
-      <h6 :class="css.logo">Feather</h6>
-    </div>
+  <header :class="css.header.classes" :style="css.header.style">
+    <h6 :class="css.logo">Feather</h6>
+    <form :class="css.form">
+      <input type="text" :class="css.input" @keyup="keyup" placeholder="Search" />
+    </form>
   </header>
 </template>
 
