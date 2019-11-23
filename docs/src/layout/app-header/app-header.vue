a93bc5abe4a7b67e003d60ff37689fe2ed2def9c
Ryan Wilke <ryanwilke@gmail.com>
2019-11-23 20:48:07 +0100

    Update app-header.vue

--- docs/src/layout/app-header/app-header.vue
+++ docs/src/layout/app-header/app-header.vue
@@ -2,7 +2,7 @@
   <header :class="css.header.classes" :style="css.header.style">
     <h6 :class="css.logo">Feather</h6>
     <form :class="css.form">
-      <input type="text" :class="css.input" @keyup="keyup" placeholder="Search" />
+      <input type="text" :class="css.input" @keyup="keyup" placeholder="Search the docs" />
     </form>
   </header>
 </template>
