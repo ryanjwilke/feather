7ccd1546f254be9ecd3d849307ed52ba18bdc034
Ryan Wilke <ryanwilke@gmail.com>
2019-11-25 18:31:59 +0100

    Cleanup

--- docs/src/components/utility-row/utility-row.vue
+++ docs/src/components/utility-row/utility-row.vue
@@ -1,10 +1,8 @@
 <template>
   <tr :class="[css.tr.base, css.tr.body]">
-    <td :class="[css.td, css.class_name]">
-      .{{ className }}
-    </td>
-    <td :class="[css.td, css.properties]">
-      <span v-for="(property, index) in properties" :key="index" class="block">
+    <td :class="[css.td.base, css.td.class_name]">.{{ className }}</td>
+    <td :class="[css.td.base, css.td.property.list]">
+      <span v-for="(property, index) in properties" :key="index" :class="css.td.property.item">
         {{ property }}: {{ value }}
       </span>
     </td>
