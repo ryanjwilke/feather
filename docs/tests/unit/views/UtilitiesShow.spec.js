ebec51b1ceb78fed7f8aea660514ac5e94a4a026
Ryan Wilke <ryanwilke@gmail.com>
2019-11-25 18:42:25 +0100

    Update the utility tests.

--- docs/tests/unit/views/UtilitiesShow.spec.js
+++ docs/tests/unit/views/UtilitiesShow.spec.js
@@ -1,32 +1,21 @@
 import { shallowMount } from '@vue/test-utils'
+import groups from '@/data/utilities.yml'
 import UtilitiesShow from '@/views/utilities/show/show.vue'
 
 describe('UtilitiesShow', () => {
-  const colorWrapper = shallowMount(UtilitiesShow, {
-    mocks: {
-      $route: {
-        params: { name: 'color' },
-      },
-    },
-  })
+  const utilities = [].concat(...groups.map((g) => g.utilities))
 
-  const bgColorWrapper = shallowMount(UtilitiesShow, {
-    mocks: {
-      $route: {
-        params: { name: 'background-color' },
-      },
-    },
-  })
-
-  it('is a Vue instance', () => {
-    expect(colorWrapper.isVueInstance()).toBeTruthy()
-  })
-
-  it('renders the color utility documentation correctly', () => {
-    expect(colorWrapper.element).toMatchSnapshot()
-  })
-
-  it('renders the background-color utility documentation correctly', () => {
-    expect(bgColorWrapper.element).toMatchSnapshot()
+  utilities.forEach((utility) => {
+    it(`renders the ${utility.name} utility documentation correctly`, () => {
+      let wrapper = shallowMount(UtilitiesShow, {
+        mocks: {
+          $route: {
+            params: { name: utility.name },
+          },
+        },
+      })
+      expect(wrapper.isVueInstance()).toBeTruthy()
+      expect(wrapper.element).toMatchSnapshot()
+    })
   })
 })
